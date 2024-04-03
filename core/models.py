205600b8812be7207020745c623cab5c19f4536f
Carlos-Coronel <cjcoronelr@fctunca.edu.py>
2024-04-03 07:55:24 -0400

    Cambios para la instalacion

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class PDF(models.Model):
     materia = models.CharField(max_length=100)
     carrera = models.CharField(max_length=100)
     codigo = models.CharField(max_length=20)
-    objetivos = models.TextField()
+    objetivos = models.TextField()#Para textos largos como fundamentos, bibliografia, etc
 
     def __str__(self):
         return self.archivo
